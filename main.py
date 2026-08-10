61a918a6a1d39505d23bad5bbf2c273b7ad20f8b
Vinh <phuvi301@gmail.com>
2026-08-10 22:15:48 +0700

    Small changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import discord
 import os
 from dotenv import load_dotenv
+import discord
 
 load_dotenv()
 
@@ -9,7 +9,8 @@ class Client(discord.Client):
         print(f'Logged on as {self.user}!')
         # self.get_all_members() trả về danh sách discord.Member (có chứa thuộc tính status)
         members = list({m.id: m for m in self.get_all_members()}.values())
-        print(f'Online users: {sum(1 for user in members if user.status == discord.Status.online)}')
+        print(f'Online users: {sum(1 for user in members if user.status == discord.Status.online and not user.bot)}')
+        print(f'Online bots: {sum(1 for user in members if user.status == discord.Status.online and user.bot)}')
         print(f'Idle users: {sum(1 for user in members if user.status == discord.Status.idle)}')
         print(f'Do not disturb users: {sum(1 for user in members if user.status == discord.Status.dnd)}')
         print(f'Offline users: {sum(1 for user in members if user.status == discord.Status.offline)}')
@@ -20,7 +21,9 @@ class Client(discord.Client):
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
         if message.content == "!hello":
-            await message.channel.send("Hello!")
+            await message.channel.send(f"Hello <@!{message.author.id}>!")
+        if "daniel" in message.content.lower():
+            await message.channel.send(f"120")
 
 
 # Bot setup
